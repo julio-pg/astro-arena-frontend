50f2b3056ccfb993f07dfc7e45e70e012f550094
Julio Patiño <franjp132@gmail.com>
2025-02-21 00:38:04 -0500

    Update styling for battles history display and enhance text formatting

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -31,12 +31,12 @@ function Index() {
             Start Game
           </Link>
         </div>
-        <div className="rounded-lg p-6 row-span-2 animate-intro-fade-up  bg-from-indigo-900 backdrop-blur-sm shadow-lg  max-sm:overflow-x-scroll">
+        <div className="rounded-lg p-6 row-span-2 animate-intro-fade-up  bg-indigo-900 bg-opacity-50 backdrop-blur-sm shadow-lg  max-sm:overflow-x-scroll">
           <div className="text-4xl font-bold">Battles Historial</div>
           <div className="flex flex-col gap-4 min-w-[30rem]">
-            <div className="flex font-semibold text-lg border-b border-white py-3">
+            <div className="flex font-bold text-lg border-b border-white py-3">
               <p className="w-1/4">Date</p>
-              <p className="w-1/4">Winner</p>
+              <p className="w-1/4 ">Winner</p>
             </div>
             {lastBattles.map((battle) => (
               <BattleRow key={battle.id} battle={battle} />
